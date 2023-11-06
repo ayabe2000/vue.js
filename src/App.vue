7a1cbd32c738f6cad312abaafaaf62eb50c19768
rina ayabe <rina@ngen.co.jp>
2023-11-06 11:27:32 +0900

    tutrial14

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <PracticeComponent13 />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PracticeComponent13 from "@/components/PracticeComponent13.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PracticeComponent13
   }
 }
 </script>
